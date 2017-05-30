84815ff8e496a242fe4259acee44f6b01542b66b
selma <selma@gmail.com>
2017-05-30 17:32:16 +0200

    Creation des fichier php pour separer des blocs html/php

---
+++ header.php
@@ -0,0 +1,22 @@
+<header class="smallheader">
+
+<div class="pub">
+            <img src="http://cematys.fr/modules/blockbanner/img/27c9d8c3972f0dc56fa80042ec1e2dd7.png" />
+        </div>
+        <div class="acces" <div class="appler">
+            <i class="fa fa-phone" aria-hidden="true"></i> applez-nous au :
+            <strong> 07 60 03 82 82 du Lundi au Vendredi de 09H00 à 18H00 non stop </strong>
+        </div>
+        <div class="connexion">
+
+            <div class="contactez-nous">
+                <a href="http://cematys.fr/nous-contacter" title="contactez-nous">contacter-nous</a>
+            </div>
+            <div class="identifiez-vous">
+                <a href="http//cematys.fr/mon-compte" title="identifiez-vous">connexion</a>
+            </div>
+
+        </div>
+        </div>
+        </div>
+        </header>
